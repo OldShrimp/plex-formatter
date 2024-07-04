197f6341b7b45285630d5c28491940876aac3fd3
Tyler Nex <tylernex@gmail.com>
2024-07-03 22:30:32 -0700

    added config functionality to daemon

--- plexFormatterDaemon.py
+++ plexFormatterDaemon.py
@@ -1,30 +1,27 @@
 import logging
 import daemon
+import os
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer
 from time import sleep
 import plexFormatter
 
-src = os.path.expanduser(os.path.join("~", "Downloads", "Torrents", "seeding"))
-dest = os.path.expanduser(os.path.join("~", "Videos"))
-
-
 class MyHandler(FileSystemEventHandler):
     def on_any_event(self, event):
         print(f"File {event.src_path} triggered {event.event_type}")
     def on_created(self, event):
-        plexFormatter.processPath(event.src_path, dest)
+        plexFormatter.processPath(event.src_path, plexFormatter.config.dest)
 
 # Create observer and event handler
 observer = Observer()
 event_handler = MyHandler()
 
 # Set up observer to watch a specific directory
-directory_to_watch = src
+directory_to_watch = plexFormatter.config.src
 observer.schedule(event_handler, directory_to_watch)
 
 # scan any existing files
-plexFormatter.processPath(src, dest)
+plexFormatter.processPath(plexFormatter.config.src, plexFormatter.config.dest)
 
 observer.start()
 
