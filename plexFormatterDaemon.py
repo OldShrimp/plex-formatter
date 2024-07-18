4975d9e526699388162dbee30d6e0d912c1c2618
Tyler Nex <tylernex@gmail.com>
2024-07-18 07:56:01 -0700

    updated daemon events to new config format

--- plexFormatterDaemon.py
+++ plexFormatterDaemon.py
@@ -6,25 +6,34 @@ from watchdog.observers import Observer
 from time import sleep
 import plexFormatter
 
-class MyHandler(FileSystemEventHandler):
+class CopyHandler(FileSystemEventHandler):
     def on_any_event(self, event):
         print(f"File {event.src_path} triggered {event.event_type}")
     def on_created(self, event):
         plexFormatter.processPath(event.src_path, plexFormatter.config.dest)
+class MoveHandler(FileSystemEventHandler):
+    def on_any_event(self, event):
+        print(f"File {event.src_path} triggered {event.event_type}")
+    def on_created(self, event):
+        plexFormatter.processPath(event.src_path, plexFormatter.config.dest, move=True)
 
 # Create observer and event handler
 observer = Observer()
-event_handler = MyHandler()
+copy_handler = CopyHandler()
+move_handler = MoveHandler()
 
-# Set up observer to watch a specific directory
-directory_to_watch = plexFormatter.config.src
-observer.schedule(event_handler, directory_to_watch)
-
-# scan any existing files
-plexFormatter.processPath(plexFormatter.config.src, plexFormatter.config.dest)
+# Set up observer to watch specified directories and do initial scan
+for path in plexFormatter.config.copysrc:
+    print(path)
+    observer.schedule(copy_handler, path)
+    plexFormatter.processPath(path, plexFormatter.config.dest)
+for path in plexFormatter.config.movesrc:
+    print(path)
+    observer.schedule(move_handler, path)
+    plexFormatter.processPath(path, plexFormatter.config.dest)
 
 observer.start()
-
+print("observer started")
 try:
     while True:
         sleep(10)
